eaffa61bd6b38cbc6b5d9bea0ee220a278099933
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2025-12-27 04:13:34 +0200

    retry to solve slug in production mode

--- lib/getCourseBySlug.tsx
+++ lib/getCourseBySlug.tsx
@@ -1,11 +1,10 @@
 import { supabase } from './supabaseClient';
 const getCourseBySlug = async (slug: string) => {
-  console.log('[PRODUCTION LOG] Query started for slug:', slug);
   const { data, error } = await supabase
     .from('courses')
     .select('*')
-    .eq('slug', slug)
-    .maybeSingle();
+    .ilike('slug', slug)
+    .single();
 
   if (error) throw new Error(error.message);
   return data;
