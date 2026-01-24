4f8f15f7e583d9ea27029b97a1dde20a4ef05d75
MostafaYahya773 <mostafayahya822@gmail.com@users.noreply.github.com>
2026-01-24 18:25:33 +0200

    remove revalidate from course page

--- src/app/Courses/page.tsx
+++ src/app/Courses/page.tsx
@@ -1,7 +1,7 @@
 import CoursesHero from '../_components/HeroCourse/HeroCourse';
 import AllCourses from '../_components/AllCourses/AllCourses';
 import GetCources from '../../../lib/GetCources';
-export const revalidate = 60;
+// export const revalidate = 60;
 export default async function Courses() {
   // get data from server
   const data = await GetCources();
