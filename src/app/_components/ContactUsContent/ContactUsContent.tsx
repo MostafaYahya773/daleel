e2d7c8fc2a82ac531ebbe8fd1f9f1e2ce82b06b2
MostafaYahya773 <programming@skygate.com>
2026-04-15 23:33:53 +0200

    edit in contact page

--- src/app/_components/ContactUsContent/ContactUsContent.tsx
+++ src/app/_components/ContactUsContent/ContactUsContent.tsx
@@ -6,17 +6,22 @@ import useAddContactUs from '@/app/hook/useAddContactUs';
 import { ContactSchema } from '@/app/auth/Schema';
 import React from 'react';
 import { UserInfoProps } from '../../interfaces';
+import Link from 'next/link';
+
 interface FormField {
   name: string;
   label: string;
   type: 'text' | 'email' | 'textarea';
   placeholder: string;
 }
+
 interface Contact {
   value: string;
   label: string;
   icon: any;
+  href: string;
 }
+
 const formFields: FormField[] = [
   { name: 'name', label: 'الاسم', type: 'text', placeholder: 'اكتب اسمك' },
   {
@@ -47,30 +52,35 @@ const formFields: FormField[] = [
 
 const ContactType: Contact[] = [
   {
-    value: '00112233445',
-    label: ' الهاتف',
+    value: '+963935746472',
+    label: 'الهاتف',
     icon: PhoneCall,
+    href: 'tel:+963935746472',
   },
   {
-    value: '1122334455',
+    value: '+963935746472',
     label: 'الواتس اب',
     icon: Phone,
+    href: 'https://wa.me/963935746472',
   },
   {
-    value: '00112233445',
+    value: '+963935746472',
     label: 'التليجرام',
     icon: Send,
+    href: 'https://t.me/+963935746472',
   },
   {
     value: 'daleel@gmail.com',
     label: 'البريد الالكتروني',
     icon: Mail,
+    href: 'mailto:daleel@gmail.com',
   },
 ];
 
 const ContactUsContent = React.memo(
   ({ userInfo }: { userInfo: UserInfoProps }) => {
     const { mutate: readMessage } = useAddContactUs();
+
     const formik = useFormik<ContactUsProps>({
       initialValues: {
         name: userInfo.name || '',
@@ -93,28 +103,41 @@ const ContactUsContent = React.memo(
       validationSchema: ContactSchema,
       enableReinitialize: true,
     });
+
     return (
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 py-5 px-5">
+        {/* ===== FORM ===== */}
         <form
           onSubmit={formik.handleSubmit}
           className="w-full flex flex-col gap-4 order-2 lg:order-1 bg-secondary p-2 rounded-lg shadow-lg"
         >
-          {formFields.slice(0, 4).map((field) => (
+          {formFields.map((field) => (
             <div key={field.name} className="flex flex-col gap-2">
               <label className="text-sm font-medium text-gray-700 px-2">
                 {field.label}
                 <span className="text-red-500 text-[18px] ms-1">*</span>
               </label>
 
-              <input
-                type={field.type}
-                className="rounded-xl border border-gray-300 px-4 py-3 text-sm outline-none focus:border-therd duration-300"
-                placeholder={field.placeholder}
-                name={field.name}
-                value={formik.values[field.name as keyof ContactUsProps]}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
+              {field.type === 'textarea' ?
+                <textarea
+                  className="rounded-xl border border-gray-300 px-4 py-3 h-[150px] text-sm outline-none focus:border-therd duration-300 resize-none"
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  value={formik.values[field.name as keyof ContactUsProps]}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              : <input
+                  type={field.type}
+                  className="rounded-xl border border-gray-300 px-4 py-3 text-sm outline-none focus:border-therd duration-300"
+                  placeholder={field.placeholder}
+                  name={field.name}
+                  value={formik.values[field.name as keyof ContactUsProps]}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+              }
+
               {formik.touched[field.name as keyof ContactUsProps] &&
                 formik.errors[field.name as keyof ContactUsProps] && (
                   <span className="text-red-500 text-sm">
@@ -124,68 +147,45 @@ const ContactUsContent = React.memo(
             </div>
           ))}
 
-          {formFields[4].type === 'textarea' && (
-            <div className="flex flex-col gap-2">
-              <label className="text-sm font-medium text-gray-700 px-2">
-                {formFields[4].label}
-                <span className="text-red-500 text-[18px] ms-1">*</span>
-              </label>
-              <textarea
-                className="rounded-xl border border-gray-300 px-4 py-3 h-[150px] text-sm outline-none focus:border-therd duration-300 resize-none"
-                placeholder={formFields[4].placeholder}
-                name={formFields[4].name}
-                value={
-                  formik.values[formFields[4].name as keyof ContactUsProps]
-                }
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-              />
-              {}
-              {formik.touched[formFields[4].name as keyof ContactUsProps] &&
-                formik.errors[formFields[4].name as keyof ContactUsProps] && (
-                  <span className="text-red-500 text-sm">
-                    {formik.errors[formFields[4].name as keyof ContactUsProps]}
-                  </span>
-                )}
-            </div>
-          )}
           <button
             disabled={formik.isSubmitting}
             type="submit"
-            className=" rounded-xl bg-therd px-6 py-3 hover:opacity-80 duration-300 text-sm font-medium text-white"
+            className="rounded-xl bg-therd px-6 py-3 hover:opacity-80 duration-300 text-sm font-medium text-white"
           >
-            {formik.isSubmitting ? (
-              <span className="loaderAnimation"></span>
-            ) : (
-              <div className="flex items-center justify-center gap-2">
+            {formik.isSubmitting ?
+              <span className="loaderAnimation" />
+            : <div className="flex items-center justify-center gap-2">
                 إرسال
                 <Send size={16} />
               </div>
-            )}
+            }
           </button>
         </form>
+
+        {/* ===== CONTACT INFO ===== */}
         <div className="grid grid-cols-1 gap-3 order-1 lg:order-2">
-          {ContactType?.map((contact, index) => (
-            <div
+          {ContactType.map((contact, index) => (
+            <Link
               key={index}
-              className="flex items-center justify-start gap-3 w-full h-full rounded-xl bg-secondary p-4 shadow-md"
+              href={contact.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-start gap-3 w-full h-full rounded-xl bg-secondary p-4 shadow-md hover:opacity-80 duration-300"
             >
-              <span className="flex items-center justify-center bg-therd rounded-full w-12 h-12">
+              <span className="flex items-center justify-center bg-therd rounded-full w-12 h-12 shrink-0">
                 <contact.icon size={25} className="text-white" />
               </span>
-              <div className="flex flex-col gap-2">
-                <p className="lg:text-xl text-lg  font-medium">
-                  {contact.label}
-                </p>
-                <p className="lg:text-xl text-lg font-medium text-gray-400">
-                  {contact.value}
-                </p>
-              </div>
-            </div>
+              <span className="lg:text-xl text-lg font-medium text-fourth underline">
+                {contact.label}
+              </span>
+            </Link>
           ))}
         </div>
       </div>
     );
-  }
+  },
 );
+
+ContactUsContent.displayName = 'ContactUsContent';
+
 export default ContactUsContent;
